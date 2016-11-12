f68782c9e779252c559e579afeb4f4d2885bc01d
fisyher <egret_ksm@hotmail.com>
2016-11-12 23:37:45 +0800

    Add placeholder files and directories
    Added an API folder for Web-service APIs
    Add more structured directories and placeholder md and js files to ensure they get added.
    Added a TODO.md as the main task list document here until a better alternative is found.

---
+++ TODO.md
@@ -0,0 +1,12 @@
+#Tasklist for 0.0.1:
+---
+##Start with 3 views:
+
+```
+Song List View <---------
+|                       |
+-> Song Detail view <---| 
+|   |                   |
+|   |                   |
+|------> Dtx Info View--|
+```
---
+++ api/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+var dtxController = require('../controllers/songs.dtx.controllers.js');
+
+//TODO: 1. Define the URL relative path for API 2. Add API controllers to each defined path
+
+module.exports = router;
