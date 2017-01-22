9ab7156907cefe45125c1cc68cf6b53f2509b607
Unknown <egret_ksm@hotmail.com>
2017-01-22 23:05:43 +0800

    Added Chart Drawing scripts file
    Added the scripts file. Many updates need to be done to these scripts in near future.

---
+++ public/components/js/chartDrawing/dtxCharter.js
@@ -0,0 +1,8 @@
+/*Dtx Chart Drawing Module for Modern Browsers
+  Author: KSMING
+*/
+'use strict';
+
+var DtxModule = (function(mod){
+    //TODO: Implement the Chart drawing functions here
+}(DtxModule || {}));
